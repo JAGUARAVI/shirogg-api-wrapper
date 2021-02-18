fa8a551273451c1aed061113c7b61e63c126a62b
null <60427892+vierofernando@users.noreply.github.com>
2021-02-18 11:20:40 +0700

    add more checks

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const fetch = require('node-fetch');
 
 module.exports = {
     /**
-     * 
+     *
      * @param {String} type - The kind of sfw image you want 
      */
     sfw: async function (type) {
         return new Promise(async (resolve, reject) => {
+			if (!type || (typeof type != 'string')) return reject("Input must be a string.");
             let result = await (await fetch(`https://shiro.gg/api/images/${type.toLowerCase()}`)).json();
             if (result.code !== 200) return reject(`${result.code}: ${result.message}`);
             return resolve(result.url);
@@ -17,8 +18,9 @@ module.exports = {
      * 
      * @param {String} type - The kind of nsfw image you want 
      */
-    nsfw: async function(type) {
-        return new Promise(async (resolve,reject) =>{
+    nsfw: async function (type) {
+        return new Promise(async (resolve, reject) =>{
+			if (!type || (typeof type != 'string')) return reject("Input must be a string.");
             let result = await (await fetch(`https://shiro.gg/api/images/nsfw/${type.toLowerCase()}`)).json();
             if (result.code !== 200) return reject(`${result.code}: ${result.message}`);
             return resolve(result.url);
