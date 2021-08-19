ba3bec7954fb4e3149c192efde11edbb0e2ec0c4
AviDoesDev <81582444+AviDoesDev@users.noreply.github.com>
2021-08-19 11:31:34 +0530

    Updated api wrapper with dynamic endpoints.

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 const fetch = require('node-fetch');
 
+const endpoint = 'https://shiro.gg/api/';
+
 module.exports = {
+
     /**
      *
-     * @param {String} type - The kind of sfw image you want 
+     * @param {String} type - The kind of sfw image you want
+     * @returns {Promise<string>}
      */
-    sfw: async function (type) {
+
+    sfw: (type) => {
         return new Promise(async (resolve, reject) => {
-            if (!type || (typeof type != 'string')) return reject("Input must be a string.");
-            let result = await (await fetch(`https://shiro.gg/api/images/${type.toLowerCase()}`)).json();
-            if (result.code !== 200) return reject(`${result.code}: ${result.message}`);
-            return resolve(result.url);
+            const result = await fetch(endpoint + 'images/' + type.toString().toLowerCase()).then(res => res.json());
+
+            if (result.code === 404) return reject('Error 404: Unknown Endpoint');
+            else if (result.code === 429) return reject('Error 429: Ratelimited');
+            else if (result.code !== 200) return reject('Error: API endpoint error!');
+
+            if (!result.url) return reject('Error: API endpoint error!');
+
+            else return resolve(result.url);
         });
     },
 
     /**
-     * 
-     * @param {String} type - The kind of nsfw image you want 
+     *
+     * @param {String} type - The kind of nsfw image you want
+     * @returns {Promise<string>}
      */
-    nsfw: async function (type) {
-        return new Promise(async (resolve, reject) =>{
-            if (!type || (typeof type != 'string')) return reject("Input must be a string.");
-            let result = await (await fetch(`https://shiro.gg/api/images/nsfw/${type.toLowerCase()}`)).json();
-            if (result.code !== 200) return reject(`${result.code}: ${result.message}`);
-            return resolve(result.url);
+
+    nsfw: (type) => {
+        return new Promise(async (resolve, reject) => {
+            const result = await fetch(endpoint + 'images/' + 'nsfw/' + type.toString().toLowerCase()).then(res => res.json());
+
+            if (result.code === 404) return reject('Error 404: Unknown Endpoint');
+            else if (result.code === 429) return reject('Error 429: Ratelimited');
+            else if (result.code !== 200) return reject('Error: API endpoint error!');
+
+            if (!result.url) return reject('Error: API endpoint error!');
+
+            else return resolve(result.url);
         });
+    },
+
+    /**
+    * @returns {Promise<any>}
+    */
+
+    fetchEndpoints: () => {
+        return fetch(endpoint + 'endpoints');
     }
 }
